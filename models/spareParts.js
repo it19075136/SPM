194573336630fb0ebd313f791fa284fb6fbae6e1
it19020822 <it19020822@my.sliit.lk>
2021-08-14 12:07:30 +0530

    spare parts model added

---
+++ models/spareParts.js
@@ -0,0 +1,50 @@
+const mongoose =require('mongoose');
+const Schema = mongoose.Schema;
+
+//---Vehicle Spare Parts Schema---//
+const sparePartsSchema = new Schema({
+    condition: {
+        type: String,
+        required: true,
+        default: 'New'
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 200
+    },
+    price: {
+        type: Number
+    },
+    negotiable:{ 
+        type: Boolean
+    },
+    imageUrl:{
+        type: Array,
+        required: true
+    },
+    userId:{
+        type: String,
+        required: true
+    },
+    contactNumbers: {
+        type: Array,
+        required: true
+    },
+    publishDate: {
+        
+    }
+},{
+    timestamps: true
+});
+
+const spareParts = mongoose.model('spareParts', sparePartsSchema);
+module.exports = spareParts;
