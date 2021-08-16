867cb3a6c7a02d140e3ea484090fd721f95383d2
it19075136 <it19075136@my.sliit.lk>
2021-08-16 10:16:55 +0530

    added initial webApp setup

---
+++ webApp/riyapola/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter className="App">
+      <Switch>
+        <Route exact path='/vehicleAd/create'>
+          Hello add form
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
