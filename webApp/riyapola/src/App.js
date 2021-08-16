85c4f97d69b3488e47fd8e870c29eeca2542b1e8
it19075136 <it19075136@my.sliit.lk>
2021-08-16 23:19:20 +0530

    vehicle-add-form-initial-setup

--- webApp/riyapola/src/App.js
+++ webApp/riyapola/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import vehicleAdForm from './components/vehicleAdForm';
 
 function App() {
   return (
     <BrowserRouter className="App">
       <Switch>
-        <Route exact path='/vehicleAd/create'>
-          Hello add form
-        </Route>
+        <Route exact path='/vehicleAd/create' component={vehicleAdForm} />
         {/* Add components */}
       </Switch>
     </BrowserRouter>
