ffe3c6fd8d61ffe1f3809e2b1fab15508445a0c0
it19075136 <it19075136@my.sliit.lk>
2021-08-18 09:29:28 +0530

    vehicle ad add form changes

--- webApp/riyapola/src/components/vehicleAdForm.js
+++ webApp/riyapola/src/components/vehicleAdForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, Input, TextArea, Button, Select, Segment, Divider, Header } from 'semantic-ui-react'
+import { Form, Input, TextArea, Button, Select, Segment, Divider, Header, FeedDate } from 'semantic-ui-react'
 
 const vehicleMakeOptions = [
     { key: 't', text: 'Toyota', value: 'toyota' },
@@ -35,149 +35,237 @@ const phoneOptions = [
     { key: 'sl', text: 'Sri Lanka (+94)', value: '+94' }
 ]
 
+const validateFields = (e) => {
+    switch (e.target.name) {
+        case 'advertisementTitle':
+            return e.target.value ? {
+                content: 'Please enter a valid title',
+                pointing: 'below',
+            } : false;
+        case '2':
+            return e.target.value ? {
+
+            } : true;
+        case '3':
+            return e.target.value ? false : true;
+        case '4':
+            return e.target.value ? false : true;
+        default:
+            break;
+    }
+}
+
 export default class vehicleAdForm extends Component {
+
+    state = {
+        title : '',
+        description : '',
+        status : '',
+        year : null,
+        make : '',
+        model : '',
+        category : '',
+        bodyType: '',
+        transmission: '',
+        condition : '',
+        engineCapacity: null,
+        fuelType: '',
+        mileage: null,    
+        price: null,
+        negotiable:false,
+        imageUrls:[],
+        userId:null,
+        contactNumbers: []
+    }
+
+    handleChange = (e) => {
+        console.log(e.target.option);
+        this.setState({ [e.target.name]: e.target.value },() => {
+            console.log(this.state);
+        });
+    }
+
+    handleSubmit = () => {
+        console.log(this.state);
+    }
+
     render() {
+
         return (
             <Form className='form-centered'>
+                <Form.Field required
+                    width='16'
+                    id='title'
+                    name="title"
+                    control={Input}
+                    label='Advertisement Title'
+                    placeholder='Add an advertisement title'
+                    onChange={this.handleChange}
+                />
+                                <Form.Field required
+                                    name="category"
+                    width='16'
+                    control={Select}
+                    options={vehicleMakeOptions} // get categories
+                    label={{ children: 'Category', htmlFor: 'category' }}
+                    placeholder='Vehicle Category'
+                    search
+                    searchInput={{ id: 'category' }}
+                    onChange={this.handleChange}
+                />
+                <Form.Field required
+                                    name="make"
+                    width='16'
+                    control={Select}
+                    options={vehicleMakeOptions}
+                    label={{ children: 'Vehicle Make', htmlFor: 'vehicleMake' }}
+                    placeholder='Vehicle Make'
+                    search
+                    searchInput={{ id: 'vehicleMake' }}
+                    onChange={this.handleChange}
+                />
+                <Form.Field required
+                                    name="model"
+                    width='16'
+                    control={Select}
+                    options={vehicleModelOptions}
+                    label={{ children: 'Vehicle Model', htmlFor: 'vehicleModel' }}
+                    placeholder='Vehicle Model'
+                    search
+                    searchInput={{ id: 'vehicleModel' }}
+                    onChange={this.handleChange}
+                />
+                <Form.Field required
+                    name='year'
+                    width='16'
+                    control={Input}
+                    type='date'
+                    label={{ children: 'Registered year', htmlFor: 'date' }}
+                    placeholder='Registered year'
+                    search
+                    searchInput={{ id: 'date' }}
+                />
+                <Form.Field required
+                    name= 'bodyType'
+                    width='16'
+                    control={Select}
+                    options={vehicleBodyOptions}
+                    label={{ children: 'Vehicle Body Type', htmlFor: 'bodyType' }}
+                    placeholder='Vehicle Body Type'
+                    search
+                    searchInput={{ id: 'bodyType' }}
+                    onChange={this.handleChange}
+                />
+                <Form.Field required
+                    name='transmission'
+                    width='16'
+                    control={Select}
+                    options={transmissionOptions}
+                    label={{ children: 'Transmission', htmlFor: 'transmission' }}
+                    placeholder='Transmission'
+                    search
+                    searchInput={{ id: 'transmission' }}
+                    onChange={this.handleChange}
+                />
+                <Form.Group>
                     <Form.Field required
-                        width='16'
-                        id='advertisementTitle'
+                        id='engineCapacity'
+                        name='engineCapacity'
+                        type='number'
                         control={Input}
-                        label='Advertisement Title'
-                        placeholder='Add an advertisement title'
-                        error={{
-                            content: 'Please enter a valid title',
-                            pointing: 'below',
-                        }}
+                        label='Engine capacity'
+                        placeholder='Engine capacity(cc)'
                     />
                     <Form.Field required
-                        width='16'
+                        name='fuelType'
                         control={Select}
-                        options={vehicleMakeOptions}
-                        label={{ children: 'Vehicle Make', htmlFor: 'vehicleMake' }}
-                        placeholder='Vehicle Make'
+                        options={fuelOptions}
+                        label={{ children: 'Fuel Type', htmlFor: 'fuelType' }}
+                        placeholder='Fuel Type'
                         search
-                        searchInput={{ id: 'vehicleMake' }}
+                        searchInput={{ id: 'fuelType' }}
+                        onChange={this.handleChange}
                     />
+                </Form.Group>
+                <Form.Field
+                    name='description'
+                    width='16'
+                    id='description'
+                    control={TextArea}
+                    label='Description'
+                    placeholder='Description'
+                    onChange={this.handleChange}
+                />
+                <Form.Group>
                     <Form.Field required
-                        width='16'
-                        control={Select}
-                        options={vehicleModelOptions}
-                        label={{ children: 'Vehicle Model', htmlFor: 'vehicleModel' }}
-                        placeholder='Vehicle Model'
-                        search
-                        searchInput={{ id: 'vehicleModel' }}
-                    />
-                    <Form.Field required
-                        width='16'
-                        control={Select}
-                        options={vehicleMakeOptions}
-                        label={{ children: 'Vehicle Make', htmlFor: 'vehicleMake' }}
-                        placeholder='Vehicle Make'
-                        search
-                        searchInput={{ id: 'vehicleMake' }}
-                    />
-                    <Form.Field required
-                        width='16'
-                        control={Select}
-                        options={vehicleBodyOptions}
-                        label={{ children: 'Vehicle Body Type', htmlFor: 'bodyType' }}
-                        placeholder='Vehicle Body Type'
-                        search
-                        searchInput={{ id: 'bodyType' }}
-                    />
-                    <Form.Field required
-                        width='16'
-                        control={Select}
-                        options={transmissionOptions}
-                        label={{ children: 'Transmission', htmlFor: 'transmission' }}
-                        placeholder='Transmission'
-                        search
-                        searchInput={{ id: 'transmission' }}
-                    />
-                    <Form.Group>
-                        <Form.Field required
-                            id='engineCapacity'
-                            type='number'
-                            control={Input}
-                            label='Engine capacity'
-                            placeholder='Engine capacity(cc)'
-                        />
-                        <Form.Field required
-                            control={Select}
-                            options={fuelOptions}
-                            label={{ children: 'Fuel Type', htmlFor: 'fuelType' }}
-                            placeholder='Fuel Type'
-                            search
-                            searchInput={{ id: 'fuelType' }}
-                        />
-                    </Form.Group>
-                    <Form.Field
-                        width='16'
-                        id='description'
-                        control={TextArea}
-                        label='Description'
-                        placeholder='Description'
-                    />
-                    <Form.Group>
-                        <Form.Field required
-                            id='mileage'
-                            type='number'
-                            control={Input}
-                            label='Mileage'
-                            placeholder='Mileage(Kms)'
-                        />
-                        <Form.Field
-                            id='price'
-                            type='number'
-                            control={Input}
-                            label='Price'
-                            placeholder='Price(Kms)'
-                        />
-                    </Form.Group>
-                    <Form.Group inline>
-                        <Form.Radio
-                            label='Registered'
-                            value='registered'
-                        // checked={value === 'registered'}
-                        // onChange={this.handleChange}
-                        />
-                        <Form.Radio
-                            label='Unregistered'
-                            value='unregistered'
-                        // checked={value === 'unregistered'}
-                        // onChange={this.handleChange}
-                        />
-                        <Form.Checkbox label='Negotiable' />
-                    </Form.Group>
-                    <Divider section />
-                    <Header as='h3'>Contact Details</Header>
-                    <Form.Group>
-                        <Form.Field required
-                            id='phoneCode'
-                            control={Select}
-                            options={phoneOptions}
-                            label='Code'
-                            placeholder='Code'
-                        />
-                        <Form.Field
-                            id='phone'
-                            control={Input}
-                            label='Phone number'
-                            placeholder='77-xxxxxxx'
-                        />
-                    </Form.Group>
-                    <Form.Field
-                        id='add'
-                        control={Button}
-                        content='Add'
+                        name='mileage'
+                        id='mileage'
+                        type='number'
+                        control={Input}
+                        label='Mileage'
+                        placeholder='Mileage(Kms)'
+                        onChange={this.handleChange}
                     />
                     <Form.Field
-                        className='form-submit-btn'
-                        id='submit'
-                        control={Button}
-                        content='Post Ad'
+                        name='price'
+                        id='price'
+                        type='number'
+                        control={Input}
+                        label='Price'
+                        placeholder='Price(Rs)'
+                        onChange={this.handleChange}
                     />
+                </Form.Group>
+                <Form.Group inline>
+                    <Form.Radio
+                        name='registered'
+                        label='Registered'
+                        value='registered'
+                        checked={this.state.condition === 'registered'}
+                        onChange={this.handleChange}
+                    />
+                    <Form.Radio
+                        name='unregistered'
+                        label='Unregistered'
+                        value='unregistered'
+                        checked={this.state.condition === 'unregistered'}
+                        onChange={this.handleChange}
+                    />
+                    <Form.Checkbox label='Negotiable'
+                        name='negotiable'
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>
+                <Divider section />
+                <Header as='h3'>Contact Details</Header>
+                <Form.Group>
+                    <Form.Field required
+                        id='phoneCode'
+                        control={Select}
+                        options={phoneOptions}
+                        label='Code'
+                        placeholder='Code'
+                    />
+                    <Form.Field
+                        id='phone'
+                        name='phone'
+                        control={Input}
+                        label='Phone number'
+                        placeholder='77-xxxxxxx'
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>
+                <Form.Field
+                    id='add'
+                    control={Button}
+                    content='Add'
+                />
+                <Form.Field
+                    className='form-submit-btn'
+                    id='submit'
+                    control={Button}
+                    content='Post Ad'
+                />
             </Form>
         )
     }
