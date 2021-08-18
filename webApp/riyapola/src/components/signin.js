34ce5f6a142abe9ad84645c4535b9270bd767028
it19390260 <it19390260@my.sliit.lk>
2021-08-18 22:18:11 +0530

    new components

--- webApp/riyapola/src/components/signin.js
+++ webApp/riyapola/src/components/signin.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Checkbox, Form } from 'semantic-ui-react'
+import GoogleLogin from 'react-google-login'
 
-const signin = () => (
+function Signin  () { 
+  const [user, setUser] = useState({
+     email :"" ,
+     password:""
+   
+})
+const responseGoogle =(response)=>{
+setUser({
+ email:response.email,
+})
+}
+const formHandler =(e)=>{
+// setUser({
+//   ...this.user,
+//   [e.target.name]:e.target.value
+// })
+setUser(prevState => ({
+ ...prevState,
+ [e.target.name]:e.target.value
+}));
+}
+const submitHandler=()=>{
+
+}
+  return(
   <Form className='form-centered'>
     <Form.Field>
       <label>Email</label>
-      <input placeholder='Email' />
+      <input placeholder='Email' name="email" onChange={formHandler}/>
     </Form.Field>
     <Form.Field>
       <label>Password</label>
-      <input placeholder='Password' />
+      <input placeholder='Password' name="password" onChange={formHandler}/>
     </Form.Field>
-    <Button type='submit'>SignIn</Button>
+    <Button type='submit' onClick={submitHandler}>SignIn</Button>
+    <GoogleLogin
+    clientId=""
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+    />
   </Form>
 )
+}
 
-export default signin
+export default Signin
