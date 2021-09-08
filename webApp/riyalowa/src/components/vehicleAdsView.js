0466a7a39e2c354f9c83652dd066cb89386ae0b0
it19075136 <it19075136@my.sliit.lk>
2021-09-08 09:17:23 +0530

    files added for view page

--- webApp/riyalowa/src/components/vehicleAdsView.js
+++ webApp/riyalowa/src/components/vehicleAdsView.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getPublishedVehicleAds } from '../redux/actions/vehicleAdActions';
+import { getPublishedVehicleAds, getVehicleAdById } from '../redux/actions/vehicleAdActions';
 
 class vehicleAdsView extends Component {
+
+    state = {
+        vehicleAds: []
+    }
+
+    componentDidMount = () => {
+
+        this.props.getPublishedVehicleAds().then((res) => {
+            console.log(res);
+            console.log(this.props.vehicleAds);
+            for (let index = 0; index < 2; index++) {
+                this.props.getVehicleAdById(this.props.vehicleAds[index]._id).then((res) => {
+                    this.setState({ vehicleAds: [...this.state.vehicleAds,res] }, () => {
+                        console.log(this.state.vehicleAds)
+                    })
+                })
+            }
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
     render() {
         return (
-            <div>
-                
+            <div style={{margin: '0 auto'}}>
+                {this.state.vehicleAds.length > 0 ? this.state.vehicleAds.map((item) => {
+                    return <div>
+                    <h2>{item.title}</h2>
+                    <img src={item.images[0]['data_url']} alt="" width="100" height="100" />
+                    </div>
+                }) : 'loading...'}
             </div>
         )
     }
 }
 
-export default connect(null,null)(vehicleAdsView);
+const mapStateToProps = (state) => ({
+    vehicleAds: state.vehicle.publishedVehicleAdIds
+})
+
+export default connect(mapStateToProps, { getPublishedVehicleAds, getVehicleAdById })(vehicleAdsView);
