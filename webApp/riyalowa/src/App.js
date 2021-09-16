7ac5319df1c2c6126f39ab00c2fb76f8a692a883
it19020822 <it19020822@my.sliit.lk>
2021-09-16 21:35:51 +0530

    reducer changes spareparts

--- webApp/riyalowa/src/App.js
+++ webApp/riyalowa/src/App.js
@@ -32,7 +32,7 @@ function App() {
         <Route exact path='/signup' component={Signup} />
         <Route exact path='/signin' component={Signin} />
         <Route exact path='/sparePartsAd/create' component={sparePartAdForm}/>
-        <Route exact path='/sparepartAds' component={sparepartsAdView}/>
+        <Route exact path='/sparePartsAds' component={sparepartsAdView}/>
         <Route exact path='/sparePartsAd/update/:id' component={updateSparePartsAdForm} />
         <Route exact path='/userProfile' component={UserProfile}/>
         <Route exact path='/category/add' component={AddCategoryForm} />
