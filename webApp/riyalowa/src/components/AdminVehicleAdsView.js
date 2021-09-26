881c285cbb80d94fe99013eb7cb0efd781b8f536
it19075136 <it19075136@my.sliit.lk>
2021-09-26 19:25:53 +0530

    admin and user action changes

--- webApp/riyalowa/src/components/AdminVehicleAdsView.js
+++ webApp/riyalowa/src/components/AdminVehicleAdsView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { updateVehicleAd, getPendingVehicleAds } from '../redux/actions/vehicleAdActions';
+import { updateVehicleAd, getPendingVehicleAds, getPublishedVehicleAds } from '../redux/actions/vehicleAdActions';
 import { connect } from 'react-redux';
 import { Button, Checkbox, Icon, Table, Header, Loader } from 'semantic-ui-react';
 import { CSVLink } from "react-csv";
@@ -26,6 +26,8 @@ class AdminVehicleAdsView extends Component {
                 alert('Please check your network connection and refresh the page')
             });
         })
+
+        this.props.getPublishedVehicleAds();
     }
 
     navigateToDetails = (id) => {
@@ -45,30 +47,16 @@ class AdminVehicleAdsView extends Component {
     }
  
     headers = [
-      { label: "First Name", key: "firstName" },
-      { label: "Last Name", key: "lastName" },
-      { label: "Email", key: "email" },
-      { label: "Age", key: "age" }
+      { label: "Ad title", key: "title" },
+      { label: "Posted Date", key: "updatedAt" },
+      { label: "Seller's Name", key: "userId" },
+      { label: "Status", key: "status" }
     ];
-     
-    data = [
-      { firstName: "Warren", lastName: "Morrow", email: "sokyt@mailinator.com", age: "36" },
-      { firstName: "Gwendolyn", lastName: "Galloway", email: "weciz@mailinator.com", age: "76" },
-      { firstName: "Astra", lastName: "Wyatt", email: "quvyn@mailinator.com", age: "57" },
-      { firstName: "Jasmine", lastName: "Wong", email: "toxazoc@mailinator.com", age: "42" },
-      { firstName: "Brooke", lastName: "Mcconnell", email: "vyry@mailinator.com", age: "56" },
-      { firstName: "Christen", lastName: "Haney", email: "pagevolal@mailinator.com", age: "23" },
-      { firstName: "Tate", lastName: "Vega", email: "dycubo@mailinator.com", age: "87" },
-      { firstName: "Amber", lastName: "Brady", email: "vyconixy@mailinator.com", age: "78" },
-      { firstName: "Philip", lastName: "Whitfield", email: "velyfi@mailinator.com", age: "22" },
-      { firstName: "Kitra", lastName: "Hammond", email: "fiwiloqu@mailinator.com", age: "35" },
-      { firstName: "Charity", lastName: "Mathews", email: "fubigonero@mailinator.com", age: "63" }
-    ];
-     
+          
     csvReport = {
-      data: this.data,
+      data: [...this.props.vehicleAds],
       headers: this.headers,
-      filename: 'Clue_Mediator_Report.csv'
+      filename: 'allVehicleAds.csv'
     };
      
     render() {
@@ -130,8 +118,9 @@ class AdminVehicleAdsView extends Component {
 }
 
 const mapStateToProps = state => ({
-    vehicleAds: state.vehicle.vehicleAds
+    vehicleAds: state.vehicle.vehicleAds,
+    publishVehicleAds: state.vehicle.publishedVehicleAdIds
 });
 
-export default connect(mapStateToProps, { updateVehicleAd, getPendingVehicleAds })(AdminVehicleAdsView)
+export default connect(mapStateToProps, { updateVehicleAd, getPendingVehicleAds, getPublishedVehicleAds })(AdminVehicleAdsView)
 
